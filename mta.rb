80d436e956031978b3b55b7e893b6dd7f85048f7
Denis Doan <dedoin08@yahoo.de>
2015-08-27 19:29:56 +0100

    Calculation of one single line works

--- mta.rb
+++ mta.rb
@@ -1,24 +1,42 @@
 trains = {
-  "N Line" => {
-    :stops => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th" ],
-  },
-  "L Line" => {
-    :stops => ["8th", "6th", "Union Square", "3rd", "1st"],
-  },
-  "6 Line" => {
-    :stops => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"],
-  },
+  
+    :Nline => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th" ],
+  
+  
+    :Lline => ["8th", "6th", "Union Square", "3rd", "1st"],
+  
+  
+    :SixLine => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"],
+  
 
 };
 
-trains["N Line"][:stops]
+
 puts " 1. Choose your Line.
        2. Your current station 
-       3. Your destination station.
+       3. Your destination Line stop.
+       4. Your destination stop.
        Here are the Lines you can choose from:
        #{trains.keys}"
 
 answer = gets.chomp 
-           if answer == "N Line"
-puts 'Here are the current stations you can choose'
+           #userStop = gets.chomp
+  if answer == "N Line"  
+  puts "Here are the current stations you can choose from : #{trains[:Nline]} "
+  stops = gets.chomp
+  start = trains[:Nline].index(stops)
+  puts "What's your destination Line?"
+        if answer == "N Line"  
+  puts "Here are the destination stations you can choose from : #{trains[:Nline]} "
+  stop = gets.chomp
+  endd = trains[:Nline].index(stop)
+  puts endd-start
+
+          
+      end      
+  elsif answer == "L Line "
+puts "Here are the current stations you can choose from: #{trains[:Lline]}"
+elsif answer == "6 Line "
+puts "Here are the current stations you can choose from: #{trains[:SixLine]}"
+else puts "Wrong choice"
 end         
